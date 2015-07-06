6e6d89bfdea2769f5fa3952dc18c4a5f52cdb1d9
chengfei <chengfei609@163.com>
2015-07-07 00:14:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chengfei/ch05_textview/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.chengfei.ch05_textview;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //设置textview4
+        TextView view = (TextView)findViewById(R.id.textview4);
+        view.setTextSize(30);
+        view.setBackgroundColor(Color.BLACK);
+        view.setText("我是在程序中设置的!");
+        view.setTextColor(Color.WHITE);
+        view.setPadding(40,40,40,40);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
